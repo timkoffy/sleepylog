c6b175a39ea849a5de51c1b5bd487aa130763c38
timkoffy <nikitakirilltimur@gmail.com>
2025-02-21 20:09:48 +0400

    кнопка клир + растягивание по горизонтали

--- sleepylog_main.py
+++ sleepylog_main.py
@@ -1,4 +1,3 @@
-
 from fileinput import close
 from locale import windows_locale
 from os import write
@@ -13,29 +12,24 @@ root.resizable(width=1, height=1)
 
 pyglet.font.add_file('Itim-Regular.ttf')
 
-
+coefficient_x=0
 
 def day_to_y(day):
     y1=day*20
     y0=y1-20
     return f'{y0},{y1}'
 
-def boxDraw(x0,x1,y):
-    #box = canvas3.create_rectangle(x0,y0,x1,y1, fill=colorBoxID, outline=colorBoxOutlineID, width=3)
+def box_draw(x0,x1,y):
     y=y.split(',')
     y0=y[0]
     y1 = y[1]
-
     box = canvas3.create_rectangle(x0, y0, x1, y1, fill=colorBoxID, width=0)
 
-def boxDraw_back(x0,x1,y):
-    #box = canvas3.create_rectangle(x0,y0,x1,y1, fill=colorBoxID, outline=colorBoxOutlineID, width=3)
+def box_draw_back(x0,x1,y):
     y=y.split(',')
     y0=int(y[0])
     y1=int(y[1])
-
     box = canvas3.create_rectangle(x0-3, y0-3, x1+3, y1+3, fill=colorBoxOutlineID, width=0)
-
 
 colorID = '#555258'
 colorID_High = '#F0F0F0'
@@ -46,33 +40,12 @@ colorBoxOutlineID = '#9000FF'
 padLabelX=3
 padLabelY=1
 
-
 canvas1=Canvas(bg="#201B24",  width=420, height=40)
-canvas1.pack(anchor=CENTER, expand=0)
+canvas1.pack(anchor=N, expand=0, fill=X)
 canvas1.create_text(210, 20, font="Itim 20", anchor='center', text="Sleepy log", fill=colorID_High)
 
 canvas2=Canvas(bg="#201B24",  width=420, height=40)
-canvas2.pack(anchor=CENTER, expand=0)
-canvas2.create_text(20, 21, font="Itim 10", anchor='center', text="19", fill=colorID)
-canvas2.create_text(40, 21, font="Itim 10", anchor='center', text="20", fill=colorID)
-canvas2.create_text(60, 21, font="Itim 10", anchor='center', text="21", fill=colorID)
-canvas2.create_text(80, 21, font="Itim 10", anchor='center', text="22", fill=colorID)
-canvas2.create_text(100, 21, font="Itim 10", anchor='center', text="23", fill=colorID_High)
-canvas2.create_text(120, 20, font="Itim 12", anchor='center', text="0", fill=colorID)
-canvas2.create_text(140, 20, font="Itim 12", anchor='center', text="1", fill=colorID)
-canvas2.create_text(160, 20, font="Itim 12", anchor='center', text="2", fill=colorID)
-canvas2.create_text(180, 20, font="Itim 12", anchor='center', text="3", fill=colorID)
-canvas2.create_text(200, 20, font="Itim 12", anchor='center', text="4", fill=colorID)
-canvas2.create_text(220, 20, font="Itim 12", anchor='center', text="5", fill=colorID)
-canvas2.create_text(240, 20, font="Itim 12", anchor='center', text="6", fill=colorID)
-canvas2.create_text(260, 20, font="Itim 12", anchor='center', text="7", fill=colorID_High)
-canvas2.create_text(280, 20, font="Itim 12", anchor='center', text="8", fill=colorID)
-canvas2.create_text(300, 20, font="Itim 12", anchor='center', text="9", fill=colorID)
-canvas2.create_text(320, 21, font="Itim 10", anchor='center', text="10", fill=colorID)
-canvas2.create_text(340, 21, font="Itim 10", anchor='center', text="11", fill=colorID)
-canvas2.create_text(360, 21, font="Itim 10", anchor='center', text="12", fill=colorID)
-canvas2.create_text(380, 21, font="Itim 10", anchor='center', text="13", fill=colorID)
-canvas2.create_text(400, 21, font="Itim 10", anchor='center', text="14", fill=colorID)
+canvas2.pack(anchor=N, expand=0, fill=X)
 
 canvas3=Canvas(bg="#201B24",  width=420, height=200,scrollregion=(0,0,5000,5000))
 vbar=Scrollbar(canvas3, orient=VERTICAL)
@@ -83,28 +56,6 @@ canvas3.pack(anchor=CENTER,expand=1,fill=BOTH)
 
 canvas3.create_line(100,0,100,5000,fill=colorID, width=3, dash = (10, 10))
 canvas3.create_line(260,0,260,5000,fill=colorID, width=3, dash = (10, 10))
-
-def time_to_x(x):
-    if x == 19: return 20
-    if x == 20: return 40
-    if x == 21: return 60
-    if x == 22: return 80
-    if x == 23: return 100
-    if x == 0: return 120
-    if x == 1: return 140
-    if x == 2: return 160
-    if x == 3: return 180
-    if x == 4: return 200
-    if x == 5: return 220
-    if x == 6: return 240
-    if x == 7: return 260
-    if x == 8: return 280
-    if x == 9: return 300
-    if x == 10: return 320
-    if x == 11: return 340
-    if x == 12: return 360
-    if x == 13: return 380
-    if x == 14: return 400
 
 x2list=[]
 x1list=[]
@@ -123,17 +74,74 @@ with open("sleepylogday.txt", 'r') as sleepylogday_file:
     for x2_day_value in sleepylogday_file:
         x2list.append([w for w in x2_day_value.strip()])
 
-output_x1list = [time_to_x(int(''.join(sublist_x1))) for sublist_x1 in x1list]
-output_x2list = [time_to_x(int(''.join(sublist_x2))) for sublist_x2 in x2list]
-output_ylist = [int(''.join(sublist_y)) for sublist_y in ylist]
 
-for dayvalueMAIN in range(len(output_ylist)):
-    boxDraw_back(output_x1list[dayvalueMAIN],output_x2list[dayvalueMAIN],day_to_y(output_ylist[dayvalueMAIN]))
+def main_draw_func(event):
+    canvas3.update()
+    global coefficient_x
+    coefficient_x = root.winfo_width() / 420
+    print(coefficient_x)
 
-for dayvalueMAIN in range(len(output_ylist)):
-    boxDraw(output_x1list[dayvalueMAIN],output_x2list[dayvalueMAIN],day_to_y(output_ylist[dayvalueMAIN]))
+    def time_to_x(x):
+        times = [19, 20, 21, 22, 23, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+        for d in range(1, 20):
+            if x == times[d]:
+                return (20 * d + 20) * coefficient_x
+
+    output_x1list = [time_to_x(int(''.join(sublist_x1))) for sublist_x1 in x1list]
+    output_x2list = [time_to_x(int(''.join(sublist_x2))) for sublist_x2 in x2list]
+    output_ylist = [int(''.join(sublist_y)) for sublist_y in ylist]
+
+    canvas1.delete('all')
+    canvas2.delete('all')
+    canvas3.delete('all')
+
+    canvas1.create_text(210*coefficient_x, 20, font="Itim 20", anchor='center', text="Sleepy log", fill=colorID_High)
+
+    canvas2.create_text(20*coefficient_x, 20, font="Itim 10", anchor='center', text="19", fill=colorID)
+    canvas2.create_text(40*coefficient_x, 20, font="Itim 10", anchor='center', text="20", fill=colorID)
+    canvas2.create_text(60*coefficient_x, 20, font="Itim 10", anchor='center', text="21", fill=colorID)
+    canvas2.create_text(80*coefficient_x, 20, font="Itim 10", anchor='center', text="22", fill=colorID)
+    canvas2.create_text(100*coefficient_x, 20, font="Itim 10", anchor='center', text="23", fill=colorID_High)
+    canvas2.create_text(120*coefficient_x, 20, font="Itim 10", anchor='center', text="00", fill=colorID)
+    canvas2.create_text(140*coefficient_x, 20, font="Itim 10", anchor='center', text="01", fill=colorID)
+    canvas2.create_text(160*coefficient_x, 20, font="Itim 10", anchor='center', text="02", fill=colorID)
+    canvas2.create_text(180*coefficient_x, 20, font="Itim 10", anchor='center', text="03", fill=colorID)
+    canvas2.create_text(200*coefficient_x, 20, font="Itim 10", anchor='center', text="04", fill=colorID)
+    canvas2.create_text(220*coefficient_x, 20, font="Itim 10", anchor='center', text="05", fill=colorID)
+    canvas2.create_text(240*coefficient_x, 20, font="Itim 10", anchor='center', text="06", fill=colorID)
+    canvas2.create_text(260*coefficient_x, 20, font="Itim 10", anchor='center', text="07", fill=colorID_High)
+    canvas2.create_text(280*coefficient_x, 20, font="Itim 10", anchor='center', text="08", fill=colorID)
+    canvas2.create_text(300*coefficient_x, 20, font="Itim 10", anchor='center', text="09", fill=colorID)
+    canvas2.create_text(320*coefficient_x, 20, font="Itim 10", anchor='center', text="10", fill=colorID)
+    canvas2.create_text(340*coefficient_x, 20, font="Itim 10", anchor='center', text="11", fill=colorID)
+    canvas2.create_text(360*coefficient_x, 20, font="Itim 10", anchor='center', text="12", fill=colorID)
+    canvas2.create_text(380*coefficient_x, 20, font="Itim 10", anchor='center', text="13", fill=colorID)
+    canvas2.create_text(400*coefficient_x, 20, font="Itim 10", anchor='center', text="14", fill=colorID)
+
+    canvas3.create_line(100*coefficient_x, 0, 100*coefficient_x, 5000, fill=colorID, width=3, dash=(10, 10))
+    canvas3.create_line(260*coefficient_x, 0, 260*coefficient_x, 5000, fill=colorID, width=3, dash=(10, 10))
+
+    for dayvalueMAIN in range(len(output_ylist)):
+        box_draw_back(output_x1list[dayvalueMAIN],output_x2list[dayvalueMAIN],day_to_y(output_ylist[dayvalueMAIN]))
+
+    for dayvalueMAIN in range(len(output_ylist)):
+        box_draw(output_x1list[dayvalueMAIN],output_x2list[dayvalueMAIN],day_to_y(output_ylist[dayvalueMAIN]))
+
+canvas3.bind("<Configure>",main_draw_func)
 
 def clear_all():
+    clear_root = Tk()
+    clear_root.title("warning!")
+    clear_root.geometry('460x150')
+    clear_root.resizable(width=0, height=0)
+
+    clear_text=Label(clear_root, text="all your logs WILL BE DELETED, do u understand that?", font="Itim 12")
+    clear_text.pack(expand=1)
+    clear_button=Button(clear_root, text="yes, i understand", font="Itim 12", command=clear_all_really)
+    clear_button.pack(expand=1)
+
+
+def clear_all_really():
     #очистка всех файлов
     with open('sleepylognight.txt', 'w'):
         pass
@@ -143,15 +151,10 @@ def clear_all():
         file.write('0')
     exit()
 
-
-
-
 canvas4=Canvas(bg="#201B24",  width=420, height=50)
-canvas4.pack(anchor=N,expand=0)
+canvas4.pack(anchor=N,expand=0, fill=X)
 
 btn = ttk.Button(text="clear_all",command=clear_all)
 canvas4.create_window(10, 10, anchor=NW, window=btn, width=100, height=30)
-
-
 
 root.mainloop()
